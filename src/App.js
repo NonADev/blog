e8ee15fb63645a681ab8fd39c79d3921323ef3a2
NonADev <wesley.vieira.lima.1999@gmail.com>
2021-09-18 02:06:00 -0300

    Remoção código legado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './Pages/HomePage/HomePage';
 
  class App extends Component {
